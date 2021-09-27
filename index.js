1340a1ec47fc9019121a0ba2bfededfbcb67a14d
Amy Posten <amyposten85@gmail.com>
2021-09-27 11:57:46 -0400

    commit tree c8f8e7c009958d3747ebfdbf077d957a54be3838

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const removeMain = document.getElementById(`main`);
+removeMain.remove();
+
+const newHeader = document.createElement(`h1`);
+newHeader.textContent = `YOUR-NAME is the champion`;
+newHeader.id = `victory`;
